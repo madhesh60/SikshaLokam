e03962ed5207474dfa35696d73907bc107417233
madhesh60 <madheshsasikala81@gmail.com>
2026-01-21 11:51:32 +0530

    updated backend deployment url

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 }
 
